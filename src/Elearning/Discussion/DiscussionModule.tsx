fd3f17049b2b6cad2e7fdc9463c4fe7370310b76
chahat233 <chahattaneja23@gmail.com>
2026-07-31 11:52:41 +0530

    correction

--- src/Elearning/Discussion/DiscussionModule.tsx
+++ src/Elearning/Discussion/DiscussionModule.tsx
@@ -4,6 +4,7 @@ import type{ Question } from './types';
 import QuestionForm from './QuestionForm';
 import QuestionList from './QuestionList';
 import styles from './DiscussionModule.module.css';
+import Loader2 from '../Header/Loader2';
 
 export default function DiscussionModule() {
   const [questions, setQuestions] = useState<Question[]>([]);
@@ -63,7 +64,11 @@ export default function DiscussionModule() {
   const handleQuestionCreated = (question: Question) => {
     setQuestions((prev) => [question, ...prev]);
   };
-
+  if (loading)
+    return(
+    <Loader2 />
+    )
+    
   return (
     <section className={styles.wrapper}>
       <header className={styles.header}>
